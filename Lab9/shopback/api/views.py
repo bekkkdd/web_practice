2dc1305301f9b40fbbe90a1c4ec85a52028a5b9c
bekkkdd <bekdauletushkempirov@gmail.com>
2020-03-27 13:00:55 +0600

    shop

---
+++ Lab9/shopback/api/views.py
@@ -0,0 +1,51 @@
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from api.models import Product, Category
+
+
+def product_list(request):
+    products = Product.objects.all()
+    products_json = [product.to_json() for product in products]
+    return JsonResponse(products_json, safe=False)
+
+
+def product_detail(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist as e:
+        return JsonResponse({'error': str(e)})
+    return JsonResponse(product.to_json())
+    # product_json = [products_json() for product in products]
+    # for product in products:
+    #     if product['id']==product_id:
+    #         return JsonResponse(product)
+
+    # return JsonResponse({'Error':'Product does not exist'})
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    categories_json = [category.to_json() for category in categories]
+    return JsonResponse(categories_json, safe=False)
+
+
+def category_detail(request, category_id):
+    try:
+        category = Category.objects.get(id=category_id)
+    except Category.DoesNotExist as e:
+        return JsonResponse({'error': str(e)})
+    return JsonResponse(category.to_json())
+
+
+def category_product(request, category_id):
+    products = Product.objects.all()
+    products_json = []
+    for product in products:
+        product = product.to_json()
+        if (product['category'] == category_id):
+            products_json.append(product)
+    if (len(products_json)):
+        return JsonResponse(products_json, safe=False)
+    return HttpResponse('Category does not exist')
